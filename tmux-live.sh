709619ffed5b5793b0ec6543c4e7f3880a7c7c76
merkksgit <merkks@protonmail.com>
2025-01-21 00:46:05 +0200

    script now opens new session in the backround

--- tmux-live.sh
+++ tmux-live.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 # Default port
 port=5050
 
@@ -20,24 +19,17 @@ fi
 selected_name="$(basename "$selected" | tr . _)"
 tmux_running="$(pgrep tmux)"
 
-if [[ -z "$TMUX" ]] && [[ -z "$tmux_running" ]]; then
-    # Create new session and start live-server directly in it
+if [[ -z "$tmux_running" ]]; then
+    # Create new session and start live-server without attaching
     tmux new-session -d -s "$selected_name" -c "$selected" "echo 'Starting live-server...'; live-server --port=$port --no-browser"
     sleep 1
     xdg-open "http://localhost:$port"
-    tmux attach -t "$selected_name"
     exit 0
 fi
 
 if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-    # Create new detached session and start live-server directly in it
+    # Create new detached session and start live-server
     tmux new-session -d -s "$selected_name" -c "$selected" "echo 'Starting live-server...'; live-server --port=$port --no-browser"
     sleep 1
     xdg-open "http://localhost:$port"
-fi
-
-if [[ -z "$TMUX" ]]; then
-    tmux attach -t "$selected_name"
-else
-    tmux switch-client -t "$selected_name"
 fi
